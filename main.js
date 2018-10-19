2daa3b8443a7e8cf5c4e0b0015e6d30fc5322221
Maximilian Kaspar <maximilian.kaspar@namics.com>
2018-10-19 08:50:48 +0200

    Menubar added, with logo

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-$("document").ready(function(){
-    $('.fade').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear',
-        arrows: false
-    });
-
-    $('.left').click(function(){
-        $('.fade').slick('slickPrev');
+window.$ = document.querySelector.bind(document);
+window.$$ = document.querySelectorAll.bind(document);
+const allOptions = $$('.option')
+const options = $('.options')
+const bar = $('.bar')
+let focus = 0;
+allOptions.forEach((option, i) => {
+    option.addEventListener('click', () => {
+        if (focus !== i) {
+            bar.className = 'bar';
+            options.className = 'options';
+            if (focus < i) {
+                bar.classList.add('right')
+            } else {
+                bar.classList.add('left')
+            }
+            bar.classList.add(`index${i}`)
+            options.classList.add(`index${i}`)
+            focus = i;
+        }
     })
-
-    $('.right').click(function(){
-        $('.fade').slick('slickNext');
-    })
-
-});
+})
